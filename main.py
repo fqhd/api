2733f935b3d8cef6b295bfea9782e9eeff5f741d
Fahd <fahd31256@gmail.com>
2024-01-22 00:12:52 +0000

    fix(error-checking): check url fields

--- main.py
+++ main.py
@@ -31,3 +31,25 @@ def request_page():
 	ascii_image_text = asciify(image, width)
 
 	return ascii_image_text
+
+@app.route('/deepdream', methods=['GET'])
+def request_page():
+	img_url = str(request.args.get('img_url'))
+	width = str(request.args.get('width'))
+
+	if img_url == None:
+		abort(400, 'Bad Request: img_url not specified')
+
+	if width == None:
+		abort(400, 'Bad Request: width not specified')
+
+	if width > 1000:
+		abort(410, 'Bad Request: Maximum width allowed is 1000')
+
+	image = load_image_from_url(img_url)
+	if image == None:
+		abort(400, 'Bad Request: Invalid URL')
+	
+	ascii_image_text = asciify(image, width)
+
+	return ascii_image_text
